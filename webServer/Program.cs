6f41b4587a53a8875e2c2da1acfc4592a4ae98e9
Leo <l.palasiuk@gmail.com>
2025-07-21 14:08:36 +0100

    returns images and css

--- webServer/Program.cs
+++ webServer/Program.cs
@@ -45,17 +45,19 @@ public class Connection
             var received = socket.Receive(buffer);
             if (received > 0)
             {
-                string request = Encoding.UTF8.GetString(buffer);
+                string request = Encoding.ASCII.GetString(buffer);
                 Console.WriteLine($"{messagePrefix}Received Message: {request}");
-                byte[] response = Encoding.UTF8.GetBytes(ParseRequest(request));
-                socket.Send(response);
+                ParseRequest(request);
+            }
+            else
+            {
                 socket.Close();
             }
         }
         
     }
     
-    public string ParseRequest(string request)
+    public void ParseRequest(string request)
     {
         string[] requestParts = request.Split("\n");
         //type
@@ -64,15 +66,15 @@ public class Connection
         {
             case "GET":
             case "HEAD":
-                return GetRequest(requestParts);
+                GetRequest(requestParts);
                 break;
             default:
-                return "HTTP/1.1 501 Not Implemented\n";
+                SendMessage(Encoding.ASCII.GetBytes("HTTP/1.1 501 Not Implemented\n"));
                 break;
         }
     }
 
-    public string GetRequest(string[] request)
+    public void GetRequest(string[] request)
     {
         string file = request[0].Split(" ")[1];
         if (file[^1] == '/')
@@ -80,10 +82,49 @@ public class Connection
             file += "index.html"; //default directories to their index page.
         }
         string path = $"{Program.FilePath}{file}";
-        string[] lines = File.ReadAllLines(path);
+        if (!File.Exists(path))
+        {
+            SendMessage("HTTP/1.1 404 Not Found\n"u8.ToArray());
+        }
+
+        byte[] content;
+        string extension = Path.GetExtension(path);
+        string type = "";
+        switch (extension)
+        {
+            case ".html":
+                type = "text/html";
+            break;
+            case ".css":
+                type = "text/css";
+                break;
+            case ".png":
+                case ".webp":
+                case ".jpeg":
+                case ".jpg":
+                case ".bmp":
+                case ".gif":
+                    type = $"image/{extension.TrimStart('.')}";
+                    break;
+            default:
+                type = "text/plain";
+                break;
+        }
+        content = File.ReadAllBytes(path);
+        string headers = $"HTTP/1.1 200 OK\n" +
+                         $"Content-Type: {type}\n" +
+                         $"Content-Length: {content.Length}\n" +
+                         $"Server: Stoat\n"+
+                         "\n"; //break between headers and content.
+        byte[] response = Encoding.ASCII.GetBytes(headers).Concat(content).ToArray();
         
-        
-        //future: check actual content type to allow txt etc.
-        return $"HTTP/1.1 200 OK\nServer: Stoat\n\n{string.Join("\n", lines)}";
+        SendMessage(response);
+    }
+    
+    public void SendMessage(byte[] message)
+    {
+        Console.WriteLine($"{messagePrefix}Sending {Encoding.ASCII.GetString(message).Split("\n\n")[0]}");
+        socket.Send(message);
     }
 }
+
